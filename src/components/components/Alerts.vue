5c1f3ad1bc3b72ae89609b199d898676eed6c4d1
Thijs van Beek <thijs@midasweb.nl>
2017-12-11 18:15:09 +0100

    added boostrap-vue and put example in alert component

--- src/components/components/Alerts.vue
+++ src/components/components/Alerts.vue
@@ -1,5 +1,38 @@
 <template lang="html">
   <div class="">
+    <h1>Bootstrap Vue Directives</h1>
+    <b-alert show>Default Alert</b-alert>
+
+    <b-alert variant="success" show>Success Alert</b-alert>
+
+    <b-alert variant="danger"
+             dismissible
+             :show="showDismissibleAlert"
+             @dismissed="showDismissibleAlert=false">
+      Dismissible Alert!
+    </b-alert>
+
+    <b-alert :show="dismissCountDown"
+             dismissible
+             variant="warning"
+             @dismissed="dismissCountDown=0"
+             @dismiss-count-down="countDownChanged">
+      <p>This alert will dismiss after {{dismissCountDown}} seconds...</p>
+      <b-progress variant="warning"
+                  :max="dismissSecs"
+                  :value="dismissCountDown"
+                  height="4px">
+      </b-progress>
+    </b-alert>
+
+    <b-btn @click="showAlert" variant="info" class="m-1">
+      Show alert with count-down timer
+    </b-btn>
+    <b-btn @click="showDismissibleAlert=true" variant="info" class="m-1">
+      Show dismissible alert ({{showDismissibleAlert?'visible':'hidden'}})
+    </b-btn>
+    <hr>
+    <h1>Bootstrap 4 Classes</h1>
     <div class="alert alert-primary" role="alert">
       This is a primary alert—check it out!
     </div>
@@ -28,7 +61,23 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      dismissSecs: 10,
+      dismissCountDown: 0,
+      showDismissibleAlert: false,
+    };
+  },
+  methods: {
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown;
+    },
+    showAlert() {
+      this.dismissCountDown = this.dismissSecs;
+    },
+  },
+};
 </script>
 
 <style lang="css">
