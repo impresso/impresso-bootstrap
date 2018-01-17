ff2df7e1c55fb7d4d14cbca8c315a03f8930e05d
Paul <paul.schroeder@uni.lu>
2018-01-17 12:07:55 +0100

    filterBox component : added contextual menu, improved styling

--- src/components/custom/Filterbox.vue
+++ src/components/custom/Filterbox.vue
@@ -1,13 +1,25 @@
 <template lang="html">
   <div class="filterbox">
     <div class="box-header">
-      <h4>fancy filter</h4>
-      <div class="remove" />
+      <div class="box-title"><h4>fancy filter</h4></div>
+      <div class="remove" v-on:click="closeBox" />
       <div class="contextmenu">
         <b-dropdown variant="link" right no-caret size="lg">
+          <div class="">
+          <b-form-select
+            :options="{ '1': 'Include', '0': 'Exclude' }"
+            :value=1>
+          </b-form-select>
+          </div>
+          <div class="dropdown-divider"></div>
           <b-dropdown-header>Options</b-dropdown-header>
-          <b-form-checkbox-group stacked v-model="selected" name="flavour2" :options="options">
+          <b-form-checkbox-group
+            stacked
+            :options="['Oranges', 'Apples', 'Bananas']">
           </b-form-checkbox-group>
+          <div class="dropdown-divider"></div>
+          <b-form-radio-group stacked :options="['Air', 'Courier', 'Mail']" />
+          <div class="dropdown-divider"></div>
           <b-dropdown-header>Settings</b-dropdown-header>
           <b-dropdown-item href="#">Action</b-dropdown-item>
           <b-dropdown-item href="#">Another action</b-dropdown-item>
@@ -19,21 +31,24 @@
     <div class="clearfix"></div>
     <div class="box-body">
       This is the content of the filterbox.
+      <b-form-select
+        class="mb-2 mr-sm-2 mb-sm-0"
+        :value="null"
+        :options="{ '1': 'One', '2': 'Two', '3': 'Three' }">
+        <option slot="first" :value="null">Choose...</option>
+      </b-form-select>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    closeBox: function removeMe() {
+    },
+  },
   data() {
     return {
-      selected: [], // Must be an array reference!
-      options: [
-        { text: 'Orange', value: 'orange' },
-        { text: 'Apple', value: 'apple' },
-        { text: 'Pineapple', value: 'pineapple' },
-        { text: 'Grape', value: 'grape' },
-      ],
     };
   },
 };
