54b38b1f8a441214108f0b78ad6f3f6240a91fbd
Paul <paul.schroeder@uni.lu>
2018-01-19 10:25:46 +0100

    filterbox tweaking

--- src/components/custom/Filterbox.vue
+++ src/components/custom/Filterbox.vue
@@ -1,4 +1,6 @@
 <template lang="html">
+  <div class="">
+
   <div class="filterbox">
     <div class="box-header">
       <div class="box-title"><h4>fancy filter</h4></div>
@@ -29,7 +31,7 @@
       </div>
     </div>
     <div class="clearfix"></div>
-    <div class="box-body">
+    <div class="box-body m-2">
       This is the content of the filterbox.
       <b-form-select
         class="mb-2 mr-sm-2 mb-sm-0"
@@ -39,6 +41,21 @@
       </b-form-select>
     </div>
   </div>
+  <div class="filterbox">
+    <div class="box-header">
+      <div class="box-title"><h4>fancy filter</h4></div>
+      <div class="remove" v-on:click="closeBox" />
+      <div class="contextmenu">
+      </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="box-body m-2">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script>
