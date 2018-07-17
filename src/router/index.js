f86fd386765d79d9a78dc3fe656edf0b0082e6db
Paul <hello@youtag.lu>
2018-07-17 14:31:00 +0200

    fixed branch

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-<<<<<<< HEAD
 import Home from '@/components/Home';
-=======
 // import Home from '@/components/Home';
 import FilterBox from '@/components/custom/Filterbox';
 import CustomSlider from '@/components/custom/Slider';
->>>>>>> 963ebd6931dcc2d5bee2482620bb17a5e1b7ff33
 import ComponentsAlerts from '@/components/components/Alerts';
 import ComponentsBadge from '@/components/components/Badge';
 import ComponentsCollapse from '@/components/components/Collapse';
@@ -17,146 +14,142 @@ Vue.use(Router);
 
 export default new Router({
   linkActiveClass: 'active',
-  routes: [
-    {
-      path: '/',
-<<<<<<< HEAD
-      redirect: '/home',
-    },
-    {
-      path: '/home',
-//      redirect: '/home',
-      component: Home,
-=======
-      redirect: '/overview',
-      component: Overview,
-    },
-    {
-      path: '/overview',
-      component: Overview,
->>>>>>> 963ebd6931dcc2d5bee2482620bb17a5e1b7ff33
-    },
-    {
-      path: '/layout/grid',
+  routes: [{
+    path: '/',
+    redirect: '/home',
+  },
+  {
+    path: '/home',
+      //      redirect: '/home',
+    component: Home,
+    redirect: '/overview',
+    component: Overview,
+  },
+  {
+    path: '/overview',
+    component: Overview,
+  },
+  {
+    path: '/layout/grid',
       // component: LayoutGrid,
-    },
-    {
-      path: '/layout/media-object',
+  },
+  {
+    path: '/layout/media-object',
       // component: LayoutMediaObject,
-    },
-    {
-      path: '/content/typography',
-      component: ContentTypography,
-    },
-    {
-      path: '/content/code',
+  },
+  {
+    path: '/content/typography',
+    component: ContentTypography,
+  },
+  {
+    path: '/content/code',
       // component: ContentCode,
-    },
-    {
-      path: '/content/images',
+  },
+  {
+    path: '/content/images',
       // component: ContentImages,
-    },
-    {
-      path: '/content/tables',
+  },
+  {
+    path: '/content/tables',
       // component: ContentTables,
-    },
-    {
-      path: '/content/figures',
+  },
+  {
+    path: '/content/figures',
       // component: ContentFigures,
-    },
-    {
-      path: '/custom/filterbox',
-      component: FilterBox,
-      name: 'FilterBox',
-    },
-    {
-      path: '/custom/slider',
-      component: CustomSlider,
-      name: 'CustomSlider',
-    },
-    {
-      path: '/components/alerts',
-      component: ComponentsAlerts,
-    },
-    {
-      path: '/components/badge',
-      component: ComponentsBadge,
-    },
-    {
-      path: '/components/breadcrumb',
+  },
+  {
+    path: '/custom/filterbox',
+    component: FilterBox,
+    name: 'FilterBox',
+  },
+  {
+    path: '/custom/slider',
+    component: CustomSlider,
+    name: 'CustomSlider',
+  },
+  {
+    path: '/components/alerts',
+    component: ComponentsAlerts,
+  },
+  {
+    path: '/components/badge',
+    component: ComponentsBadge,
+  },
+  {
+    path: '/components/breadcrumb',
       // component: ComponentsBreadcrumb,
-    },
-    {
-      path: '/components/buttons',
+  },
+  {
+    path: '/components/buttons',
       // component: ComponentsButtons,
-    },
-    {
-      path: '/components/button_group',
+  },
+  {
+    path: '/components/button_group',
       // component: ComponentsButtonGroup,
-    },
-    {
-      path: '/components/card',
+  },
+  {
+    path: '/components/card',
       // component: ComponentsCard,
-    },
-    {
-      path: '/components/carousel',
+  },
+  {
+    path: '/components/carousel',
       // component: ComponentsCarousel,
-    },
-    {
-      path: '/components/collapse',
-      component: ComponentsCollapse,
-    },
-    {
-      path: '/components/dropdowns',
+  },
+  {
+    path: '/components/collapse',
+    component: ComponentsCollapse,
+  },
+  {
+    path: '/components/dropdowns',
       // component: ComponentsDropdowns,
-    },
-    {
-      path: '/components/forms',
+  },
+  {
+    path: '/components/forms',
       // component: ComponentsForms,
-    },
-    {
-      path: '/components/input_group',
+  },
+  {
+    path: '/components/input_group',
       // component: ComponentsInputGroup,
-    },
-    {
-      path: '/components/jumbotron',
+  },
+  {
+    path: '/components/jumbotron',
       // component: ComponentsJumbotron,
-    },
-    {
-      path: '/components/list_group',
+  },
+  {
+    path: '/components/list_group',
       // component: ComponentsListGroup,
-    },
-    {
-      path: '/components/modal',
+  },
+  {
+    path: '/components/modal',
       // component: ComponentsModal,
-    },
-    {
-      path: '/components/navs',
+  },
+  {
+    path: '/components/navs',
       // component: ComponentsNavs,
-    },
-    {
-      path: '/components/navbar',
+  },
+  {
+    path: '/components/navbar',
       // component: ComponentsNavbar,
-    },
-    {
-      path: '/components/pagination',
+  },
+  {
+    path: '/components/pagination',
       // component: ComponentsPagination,
-    },
-    {
-      path: '/components/popovers',
+  },
+  {
+    path: '/components/popovers',
       // component: ComponentsPopovers,
-    },
-    {
-      path: '/components/progress',
+  },
+  {
+    path: '/components/progress',
       // component: ComponentsProgress,
-    },
-    {
-      path: '/components/scrollspy',
+  },
+  {
+    path: '/components/scrollspy',
       // component: ComponentsScrollspy,
-    },
-    {
-      path: '/components/tooltips',
+  },
+  {
+    path: '/components/tooltips',
       // component: ComponentsTooltips,
-    },
+  },
   ],
 });
